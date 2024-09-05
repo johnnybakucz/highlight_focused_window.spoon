00f86d882c611417e44a4745345cc1f2a46bde2d
Jonatan Bakucz <jonatan.bakucz@arsana.io>
2024-09-05 15:58:06 +0200

    fix alttab interaction

--- init.lua
+++ init.lua
@@ -8,27 +8,30 @@ obj.homepage = "https://github.com/johnnybakucz/highlight_focused_window.spoon"
 
 -- Configuration
 local borderColor = {red=1, green=0, blue=0, alpha=0.8}
-local borderWidth = 15
-local updateInterval = 0.5  -- 500 milliseconds
+local borderWidth = 10
 
 -- Global variable to store the border
 local focusBorder = nil
+
+-- Function to delete the border
+local function deleteBorder()
+    if focusBorder then
+        focusBorder:delete()
+        focusBorder = nil
+    end
+end
 
 -- Function to draw the border
 local function drawBorder()
     local win = hs.window.focusedWindow()
 
-    if focusBorder then
-        focusBorder:delete()
-        focusBorder = nil
-    end
+    deleteBorder()
 
     if not win then
         return
     end
 
     local frame = win:frame()
-    local screenFrame = win:screen():frame()
 
     -- Adjust frame for border width and padding
     local adjustedFrame = {
@@ -52,8 +55,12 @@ end
 -- Event listener for window focus changes
 local windowFilter = hs.window.filter.new()
 windowFilter:subscribe(hs.window.filter.windowFocused, drawBorder)
-windowFilter:subscribe(hs.window.filter.windowDestroyed, drawBorder)
+windowFilter:subscribe(hs.window.filter.windowUnfocused, deleteBorder)
+windowFilter:subscribe(hs.window.filter.windowDestroyed, deleteBorder)
 windowFilter:subscribe(hs.window.filter.windowMoved, drawBorder)
-windowFilter:subscribe(hs.window.filter.windowMinimized, drawBorder)
+windowFilter:subscribe(hs.window.filter.windowMinimized, deleteBorder)
+windowFilter:subscribe(hs.window.filter.windowHidden, deleteBorder)
+windowFilter:subscribe(hs.window.filter.windowUnminimized, drawBorder)
+windowFilter:subscribe(hs.window.filter.windowUnhidden, drawBorder)
 
 return obj
